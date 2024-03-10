502183658f0c0ac6c76a9dd2d397380c21b3386f
rober99.guedes@gmail.com <rober99.guedes@gmail.com>
2024-03-10 14:34:04 -0600

    aplicacion terminada com mi css

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const db = {
   methods: {
     find: (id) => {
-      return db.items.find((item) => item.id === parseInt(id));
+      return db.items.find((item) => item.id == id);
     },
     remove: (items) => {
       items.forEach((item) => {
-        const product = db.items.find(item.id);
+        const product = db.methods.find(item.id);
         product.qty = product.qty - item.qty;
       });
-      console.log(db);
     },
   },
   items: [
@@ -69,7 +68,7 @@ const shoppingCart = {
       return shoppingCart.items.reduce((acc, item) => acc + item.qty, 0);
     },
     get: (id) => {
-      const index = shoppingCart.items.findIndex((item) => item.id === id);
+      const index = shoppingCart.items.findIndex((item) => item.id == id);
       return index >= 0 ? shoppingCart.items[index] : null;
     },
     getTotal: () => {
@@ -91,12 +90,11 @@ const shoppingCart = {
 
 renderStore();
 function renderStore() {
-  console.log(db.items);
   const html = db.items.map((item) => {
     return `        <div class="item">
           <div class="title">${item.title}</div>
           <div class="price">${numberToCurrency(item.price)}</div>
-          <div class="qty">${item.qty}</div>
+          <div class="qty">${item.qty} ${item.qty > 1 ? "units" : "unit"}</div>
           <div class="action">
             <button class="add" data-id=${item.id}>Add to cart</button>
           </div>
@@ -106,12 +104,8 @@ function renderStore() {
 
   document.querySelectorAll(".item .action .add").forEach((button) => {
     button.addEventListener("click", (e) => {
-      console.log(button);
-
-      const id = e.target.getAttribute("data-id");
+      const id = parseInt(e.target.getAttribute("data-id"));
       const item = db.methods.find(id);
-
-      console.log(item);
 
       if (item && item.qty - 1 > 0) {
         shoppingCart.methods.add(id, 1);
@@ -123,11 +117,95 @@ function renderStore() {
   });
 }
 
-function renderShoppingCart() {}
+function renderShoppingCart() {
+  // shoppingCart.items.
+  const htmle = shoppingCart.items.map((item) => {
+    const dbItem = db.methods.find(item.id);
+    console.log(shoppingCart.items, dbItem);
+    return `<div class="item">
+                  <div class="title">${dbItem.title}</div>
+                  <div class="price">${numberToCurrency(dbItem.price)}</div>
+                  <div class="qty">${item.qty} unit</div>
+                  <div class="subtotal">Subtotal:${numberToCurrency(
+                    item.qty * dbItem.price
+                  )}</div>
+
+                  <div class="action">
+                       <button class="addOne" data-id=${item.id}>+</button>
+                  </div>
+                  <div class="action">
+                       <button class="removeOne" data-id=${item.id}>-</button>
+                  </div>
+            </div>`;
+  });
+  const closeButton = `
+            <div class="cart-header">
+                <button class="bClose">Close</div>
+
+            </div>
+  `;
+  const purchaseButton =
+    shoppingCart.items.length > 0
+      ? `
+            <div class="cart-actions">
+                  <button class="bPurchase">Purchase</button>
+             </div>        
+  `
+      : ``;
+
+  const total = shoppingCart.methods.getTotal();
+  const totalContainer = `
+            <div class="total">Total: ${numberToCurrency(total)}</div>
+  
+  `;
+
+  const shoppingContainer = document.querySelector("#shopping-cart-container");
+  shoppingContainer.innerHTML =
+    closeButton + htmle.join("") + totalContainer + purchaseButton;
+  shoppingContainer.classList.add("show");
+  shoppingContainer.classList.remove("hide");
+
+  document.querySelectorAll(".addOne").forEach((but) => {
+    but.addEventListener("click", (e) => {
+      let id = but.getAttribute("data-id");
+      id = parseInt(id);
+      if (shoppingCart.methods.hasInventory(id, 1)) {
+        shoppingCart.methods.add(id, 1);
+        renderShoppingCart();
+      }
+    });
+  });
+
+  document.querySelectorAll(".removeOne").forEach((but) => {
+    but.addEventListener("click", (e) => {
+      const id = parseInt(but.getAttribute("data-id"));
+      shoppingCart.methods.remove(id, 1);
+      renderShoppingCart();
+    });
+  });
+
+  const bPurchase = document.querySelector(".bPurchase");
+  if (bPurchase) {
+    bPurchase.addEventListener("click", (e) => {
+      shoppingCart.methods.purchase();
+      renderShoppingCart();
+      renderStore();
+    });
+  }
+
+  document.querySelector(".bClose").addEventListener("click", (e) => {
+    const shoppingCartContainer = document.querySelector(
+      "#shopping-cart-container"
+    );
+    console.log(shoppingCartContainer);
+    shoppingCartContainer.classList.remove("show");
+    shoppingCartContainer.classList.add("hide");
+  });
+}
 
 function numberToCurrency(n) {
   return new Intl.NumberFormat("en-US", {
-    maximumSignificantDigits: 2,
+    maximumSignificantDigits: 4,
     style: "currency",
     currency: "USD",
   }).format(n);
